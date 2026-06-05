1690b7c30e4b29b2d717337e4327c898d95dbe0f
Brandyn Tse <brandyntse941@gmail.com>
2026-06-05 21:18:43 +0800

    added asymmetric gravity, variable jumping, and variable movement. mapped to analog controls

--- assets/player.gd
+++ assets/player.gd
@@ -3,8 +3,11 @@ extends CharacterBody3D
 @export_category("Player Movement")
 @export var speed := 5.0
 @export var jump_velocity := 4.5
+@export var jump_cut_factor := 0.6
+@export var fall_gravity_multiplier := 2.0
 const ROTATION_SPEED := 6.0
 
+# TODO: add coyote time and jump buffer soon
 
 #slowly rotate the charcter to point in the direction of the camera_pivot
 #@onready var camera_pivot : Node3D = $camera_pivot
@@ -17,29 +20,43 @@ var player_animation_state : animation_state = animation_state.IDLE
 @onready var animation_player : AnimationPlayer = $"playermodel/character-male-e2/AnimationPlayer"
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Asymmetric gravity.
+	# gravity is heavier when the velocity.y is decreasing. slower rise, faster descent
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
+		if velocity.y < 0.0:
+			velocity += get_gravity() * fall_gravity_multiplier * delta
+		else:
+			velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Handle jump. We used is_action_just_pressed because is_action_pressed would create continuous jumping
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
-		#player_animation_state = animation_state.JUMPING
-		
+
+	# when player releases the jump button, the velocity.y is multiplied by a decimal to shorten the jump arc
+	# this creates variable jumping
+	# we use velocity.y > 0.0 because we want to cut the jump. if the velocity is negative (falling)
+	# then this would actually make the player linger in the air, cutting the fall instead of the jump
+	if Input.is_action_just_released("ui_accept") and velocity.y > 0.0:
+		velocity.y *= jump_cut_factor
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	# tried room_camera, but it still doesn't move as expected. will need to reread code if this doesn't work
+	var input_strength : float = minf(input_dir.length(), 1.0)
+
 	var direction = (camera_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		# makes speed of movement sensitive to how far down the stick is being tilted
+		var target_speed := speed * input_strength
+		
+		velocity.x = direction.x * target_speed
+		velocity.z = direction.z * target_speed
+		
 		#now rotate the model
 		rotate_model(direction, delta)
 		player_animation_state = animation_state.RUNNING
 	else:
+		# this is to make it slow down by the speed
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 		player_animation_state = animation_state.IDLE
